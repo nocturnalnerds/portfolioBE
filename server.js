65f340679c6975645efae8efe6184822ae723f2d
Williams Sandjaya <133097737+nocturnalnerds@users.noreply.github.com>
2026-07-08 17:04:31 +0700

    netlify support

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/.netlify/functions/api/contact", contactRouter);
+contactRouter.get('/hello', (req, res) => {
+    res.json({ message: "Hello from your Node.js API!" });
+});
+
+app.use("/.netlify/functions/", contactRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server ENV ${process.env.NODE_ENV}`);
     console.log(`Server running on ${process.env.PORT}`);
     console.log(`Mode: ${process.env.NODE_ENV}`);
 });
+
+module.exports.handler = serverless(app);
